ac130f3197090fd3f1863686fd8adf140c6278e6
DELL <bhaumik.desai.13@gmail.com>
2024-02-23 21:01:24 +0530

    Test case - 5

--- example.incubye/src/main/java/example/incubye/Calculator.java
+++ example.incubye/src/main/java/example/incubye/Calculator.java
@@ -3,25 +3,38 @@ package example.incubye;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Calculator {
-	static Pattern pattern = Pattern.compile("\\d+");
+	static Pattern pattern = Pattern.compile("-?\\d+");
+	static Predicate<Integer> isNegative = num-> num<0;
 
-	public static int add(String number) {
+	public static int add(String number) throws Exception {
 		if("".equalsIgnoreCase(number.trim())) {
 			return 0;
 		}else {
 			Matcher matcher = pattern.matcher(number);
 			
-			List<Integer> digits = new ArrayList();
+			List<Integer> digits = new ArrayList<Integer>();
 			while(matcher.find()) {
+				
 				digits.add(Integer.parseInt(matcher.group()));
 			}
 			
-			return digits.stream().collect(Collectors.summingInt(Integer::valueOf));
+			List<Integer> negativeCount = digits.stream().filter(isNegative).collect(Collectors.toList());
+			if(negativeCount.isEmpty()) {
+				return digits.stream().collect(Collectors.summingInt(Integer::valueOf));
+				
+			}else {
+				String commaSeparated= negativeCount.stream().map(String::valueOf).collect(Collectors.joining(","));
+				
+				throw new Exception("negative numbers not allowed "+commaSeparated);
+			}
+			
+			
 				
 		}
 		
--- example.incubye/src/test/java/example/incubye/CalculatorTest.java
+++ example.incubye/src/test/java/example/incubye/CalculatorTest.java
@@ -7,19 +7,30 @@ import org.junit.jupiter.api.Test;
 class CalculatorTest {
 
 	@Test
-	void test_1() {
+	void test_1() throws Exception {
 		String numbers="1,2,3,4";
 		Assertions.assertEquals(10, Calculator.add(numbers));
 	}
 	@Test
-	void test_2() {
+	void test_2() throws Exception {
 		String numbers="";
 		Assertions.assertEquals(0, Calculator.add(numbers));
 	}
 	@Test
-	void test_3() {
-		String numbers="\"1\\n2,3";
+	void test_3() throws Exception {
+		String numbers="\"1\\n2,3"; 
 		Assertions.assertEquals(6, Calculator.add(numbers));
+	}
+	@Test
+	void test_4() throws Exception {
+		String numbers="//;\\n1;2";
+		Assertions.assertEquals(3, Calculator.add(numbers));
+	}
+	@Test
+	void test_5() throws Exception {
+		String numbers="//;\\n1;2,5,-6,-5,-3";
+		Exception exception = Assertions.assertThrows(Exception.class, ()->Calculator.add(numbers));
+		Assertions.assertEquals("negative numbers not allowed -6,-5,-3", exception.getMessage());
 	}
 	
 
